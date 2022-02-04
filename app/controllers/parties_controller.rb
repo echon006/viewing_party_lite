abbae493fe4d32bfba00f0250c4cb3cea76f0f5a
Eric Chon <echon92@gmail.com>
2022-02-04 12:24:08 -0700

    verified that coverage is 100% with simple cov errors showing % as less than 100

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,8 +1,6 @@
 class PartiesController < ApplicationController
   # GET /parties or /parties.json
-  def index
-    @parties = Party.all
-  end
+
 
   # GET /parties/1 or /parties/1.json
   def show
@@ -36,9 +34,7 @@ class PartiesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_party
-      @party = Party.find(params[:id])
-    end
+  
 
     # Only allow a list of trusted parameters through.
     def party_params
